45959d9e5a8620be9673fe5fa631825d41df1650
John Shaffer <jshaffer2112@gmail.com>
2019-08-22 13:55:38 -0500

    Add project.clj.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject chesty "0.1.0-SNAPSHOT"
+  :description "Chesty is a Clojure web framework that uses sensible defaults to create websites in minutes."
+  :url "https://github.com/john-shaffer/chesty"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns chesty})
