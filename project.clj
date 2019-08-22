5a42ad02f31326999b9206fb881788a871ac231b
John Shaffer <jshaffer2112@gmail.com>
2019-08-22 14:04:00 -0500

    Add chesty.clj.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/john-shaffer/chesty"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [manila-john "0.1.0"]]
   :repl-options {:init-ns chesty})
