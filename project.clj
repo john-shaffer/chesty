7ce82bed8e5378c19b60440e811ab3593c8568ec
John Shaffer <jshaffer2112@gmail.com>
2019-09-18 08:16:36 -0500

    Version 0.2.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chesty "0.1.0"
+(defproject chesty "0.2.0-SNAPSHOT"
   :description "Chesty is a Clojure web framework that uses sensible defaults to create websites in minutes."
   :url "https://github.com/john-shaffer/chesty"
   :license {:name "The MIT License"
